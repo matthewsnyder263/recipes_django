c8449cebc5a6a0f60adbf9f5e9ee1e4725e81925
Matt Snyder <msnyd87@gmail.com>
2023-04-06 12:53:39 -0400

    Made show_recipe and recipe_list links

--- recipes/models.py
+++ recipes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
         title = models.CharField(max_length=200)
-        picture = models.URLField()
+        picture = models.URLField(blank=True)
         description = models.TextField()
         created_on = models.DateTimeField(auto_now_add=True)
         #calories = models.?
--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from recipes.views import recipe_list, show_recipe
 
 urlpatterns = [
-    path("recipes/", recipe_list),
-    path("recipes/<int:id>/", show_recipe),
+    path("recipes/", recipe_list, name="recipe_list"),
+    path("recipes/<int:id>/", show_recipe, name="show_recipe"),
 ]
