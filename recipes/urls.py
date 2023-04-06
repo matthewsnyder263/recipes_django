f63f65af99f8cd23d24e9fcce884ce1800e3fe0b
Matt Snyder <msnyd87@gmail.com>
2023-04-06 16:38:51 -0400

    added several changes. about to add base html and have yet to add my edit html though have edit infrastructure in place

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from recipes.views import recipe_list, show_recipe, create_recipe
+from recipes.views import recipe_list, show_recipe, create_recipe, edit_recipe
 
 urlpatterns = [
-    path("recipes/", recipe_list, name="recipe_list"),
-    path("recipes/<int:id>/", show_recipe, name="show_recipe"),
-    path("recipes/create/", create_recipe, name="create_recipe"),
+    path("", recipe_list, name="recipe_list"),
+    path("<int:id>/", show_recipe, name="show_recipe"),
+    path("create/", create_recipe, name="create_recipe"),
+    path("<int:id>/edit/", edit_recipe, name="edit_recipe")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -22,10 +22,25 @@ def create_recipe(request):
         form = RecipeForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("recipes_list")
+            return redirect("recipe_list")
     else:
         form = RecipeForm()
     context = {
         "form": form,
     }
     return render(request, "recipes/create.html", context)
+
+def edit_recipe(request):
+    recipe = get_object_or_404(Recipe, id=id)
+    if request.method == "POST":
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect("show_recipe", id=id)
+    else:
+        form = RecipeForm(instance=recipe)
+        context = {
+            "recipe_object": recipe,
+            "recipe_form": form,
+        }
+    return render(request, "recipes/edit.html", context)
