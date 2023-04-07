e893f5e9287be2c0c5d91b4773a1fa07e60aa431
Matt Snyder <msnyd87@gmail.com>
2023-04-06 21:07:02 -0400

    finished with 4-6 D4 Inheritance

--- recipes/forms.py
+++ recipes/forms.py
@@ -2,6 +2,8 @@ from django.forms import ModelForm
 from recipes.models import Recipe
 
 class RecipeForm(ModelForm):
+    # email_address = forms.EmailField(max_length=300)
+
     class Meta:
         model = Recipe
         fields = [
